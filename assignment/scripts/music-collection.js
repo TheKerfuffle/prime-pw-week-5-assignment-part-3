1719fdd60b71d0047d047a4c8f76018f1be63e48
Charlie Stokes <chstokes7@gmail.com>
2021-03-23 17:26:18 -0500

    finished addToCollection function

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,1 +1,16 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(album);
+}
+
+addToCollection('2112', 'Rush', 1976);
+addToCollection('Moving Pictures', 'Rush', 1981)
+console.log(collection);
