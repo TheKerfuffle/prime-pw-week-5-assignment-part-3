eedc888270c0859569b00126d29574a83a364dc2
Charlie Stokes <chstokes7@gmail.com>
2021-03-24 02:29:40 -0500

    showCollection is now functioning as intended

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -11,6 +11,25 @@ function addToCollection(title, artist, yearPublished) {
   collection.push(album);
 }
 
+function showCollection(aCollection) {
+  console.log('Number of objects in this collection:', aCollection.length);
+  for (let record in aCollection){
+    console.log(`${aCollection[record].title} by ${aCollection[record].artist}, published in ${aCollection[record].yearPublished}`);
+  }
+}
+
 addToCollection('2112', 'Rush', 1976);
+console.log(collection[collection.length-1]);
 addToCollection('Moving Pictures', 'Rush', 1981)
+console.log(collection[collection.length-1]);
+addToCollection('Running With Scissors', 'Weird Al', 1999)
+console.log(collection[collection.length-1]);
+addToCollection('Boston', 'Boston', 1976)
+console.log(collection[collection.length-1]);
+addToCollection('Cold Dark Place', 'Mastodon', 2017)
+console.log(collection[collection.length-1]);
+addToCollection('Echoes, Silence, Patience & Grace', 'Foo Fighters', 2007)
+console.log(collection[collection.length-1]);
 console.log(collection);
+
+showCollection(collection);
